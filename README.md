e252b19045a9c7c97775a854f58b2f4355eed9d5
Esterkxz <elii.esterisk@gmail.com>
2025-02-09 16:55:36 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,175 @@
+
+---
+# Korean
+<br />
+
+# JCODD
+
+JCODD는 JSON 데이터를 더 작고 효율적인 형식으로 변환하기 위한 JavaScript 라이브러리입니다. 이 라이브러리는 JSON 데이터를 압축하고, 다시 원래의 JSON 형식으로 복원하는 기능을 제공합니다. 최종적으로 BASE64로 인코드 했을 때 적은 길이의 텍스트를 얻는 것을 목표로 작성되었습니다.
+
+## 주요 기능
+
+- **JSON 데이터 압축**: JSON 데이터를 JCODD 형식으로 변환하여 더 작은 크기로 압축할 수 있습니다.
+- **JCODD 데이터 복원**: JCODD 형식의 데이터를 원래의 JSON 형식으로 복원할 수 있습니다.
+- **객체 직접 변환**: JavaScript 객체를 직접 JCODD 형식으로 변환하거나 JCODD 형식에서 객채로 변환할 수 있습니다.
+
+## 설치
+
+JCODD는 classic .js 파일로 배포됩니다.
+
+## 사용법
+
+### JSON 데이터 압축
+
+```javascript
+const json = '{"name":"Jane","age":30,"city":"Incheon"}';
+const jcodd = Jcodd.toCodd(json);
+console.log(jcodd); // 압축된 JCODD 형식의 데이터 출력
+```
+
+### JCODD 데이터 복원
+
+```javascript
+const jcodd = '...'; // JCODD 형식의 데이터
+const json = Jcodd.toJson(jcodd);
+console.log(json); // 원래의 JSON 데이터 출력
+```
+
+### 객체 직접 변환
+
+```javascript
+const obj = { name: "Jane", age: 30, city: "Incheon" };
+const jcodd = Jcodd.coddify(obj);
+console.log(jcodd); // 객체를 JCODD 형식으로 변환하여 출력
+```
+
+### JCODD 형식에서 객체로 변환
+
+```javascript
+const jcodd = '...'; // JCODD 형식의 데이터
+const obj = Jcodd.decoddify(jcodd);
+console.log(obj); // JCODD 형식의 데이터를 객체로 변환하여 출력
+```
+
+### Jcodd 객체 이용
+
+```javascript
+const origin = { name: "Jane", age: 30, city: "Incheon" };
+
+const jc = JCODD(origin);
+console.log(jc.base64); // 객체를 BASE64 JCODD 형식으로 변환하여 출력
+
+const jc1 = JCODD(jc.json);
+jc1.obj.name = "John";
+console.log(jc1.jcodd); // JSON 코드를 JCODD 형식으로 변환하여 출력
+
+const jc2 = JCODD(jc1.jcodd);
+jc2.obj.age = "29";
+console.log(jc2.json); // JCODD 코드를 JSON 형식으로 변환하여 출력
+
+const jc3 = JCODD(jc2.base64);
+jc3.obj.city = "Daejeon";
+console.log(jc3.obj); // BASE64 JCODD 형식에서 객체로 변환하여 출력
+
+console.log("data: " + jc3); // String 자동 형변환 시 JCODD 형식으로 출력
+```
+
+
+## 기여
+
+기여를 원하시면, 이 저장소를 포크하고 풀 리퀘스트를 보내주세요. 버그 리포트와 기능 요청은 이슈 트래커를 통해 제출할 수 있습니다.
+
+JS 이외 추가 언어로 포팅된 버전을 작성하여 풀 리퀘스트 하시면 프로젝트에 추가하도록 할 예정입니다.
+
+## 라이선스
+
+이 프로젝트는 MIT 라이선스 하에 배포됩니다. 자세한 내용은 `LICENSE` 파일을 참조하세요.
+
+<br />
+
+---
+# English
+<br />
+
+# JCODD
+
+JCODD is a JavaScript library for converting JSON data into a smaller and more efficient format. This library provides functionality to compress JSON data and restore it back to its original JSON format. It is designed to achieve shorter text length when encoded in BASE64.
+
+## Key Features
+
+- **JSON Data Compression**: Convert JSON data into JCODD format to compress it into a smaller size.
+- **JCODD Data Restoration**: Restore data in JCODD format back to its original JSON format.
+- **Direct Object Conversion**: Convert JavaScript objects directly to JCODD format or from JCODD format back to objects.
+
+## Installation
+
+JCODD is distributed as a classic .js file.
+
+## Usage
+
+### JSON Data Compression
+
+```javascript
+const json = '{"name":"Jane","age":30,"city":"Incheon"}';
+const jcodd = Jcodd.toCodd(json);
+console.log(jcodd); // Outputs compressed JCODD format data
+```
+
+### JCODD Data Restoration
+
+```javascript
+const jcodd = '...'; // JCODD format data
+const json = Jcodd.toJson(jcodd);
+console.log(json); // Outputs original JSON data
+```
+
+### Direct Object Conversion
+
+```javascript
+const obj = { name: "Jane", age: 30, city: "Incheon" };
+const jcodd = Jcodd.coddify(obj);
+console.log(jcodd); // Outputs object converted to JCODD format
+```
+
+### Conversion from JCODD Format to Object
+
+```javascript
+const jcodd = '...'; // JCODD format data
+const obj = Jcodd.decoddify(jcodd);
+console.log(obj); // Outputs JCODD format data converted to object
+```
+
+### Using Jcodd Object
+
+```javascript
+const origin = { name: "Jane", age: 30, city: "Incheon" };
+
+const jc = JCODD(origin);
+console.log(jc.base64); // Outputs object converted to BASE64 JCODD format
+
+const jc1 = JCODD(jc.json);
+jc1.obj.name = "John";
+console.log(jc1.jcodd); // Converts JSON code to JCODD format and outputs it
+
+const jc2 = JCODD(jc1.jcodd);
+jc2.obj.age = "29";
+console.log(jc2.json); // Converts JCODD code to JSON format and outputs it
+
+const jc3 = JCODD(jc2.base64);
+jc3.obj.city = "Daejeon";
+console.log(jc3.obj); // Converts BASE64 JCODD format to object and outputs it
+
+console.log("data: " + jc3); // Outputs JCODD format when automatically typecast to string
+```
+
+
+## Contribution
+
+If you wish to contribute, please fork this repository and send a pull request. Bug reports and feature requests can be submitted through the issue tracker.
+   
+If you port the library to additional languages other than JS and submit a pull request, we will consider adding it to the project.
+
+
+## License
+
+This project is distributed under the MIT License. See the `LICENSE` file for more details.
---
+++ jcodd.js
@@ -0,0 +1,203 @@
+/*
+
+MIT License
+
+Copyright (c) 2023 Esterkxz (Ester1 / 에스터1z)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+     
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+// JSON Characterized Object Data Definition //
+//
+// The JSON based lite code format
+//
+// v0.7 / release 2025.02.09
+//
+// Take to be liten from JSON code to smaller converted characters for like as BASE64.
+//
+//
+// :: Code regulations
+//
+// 1. null is n, true is t, false is f.
+// 2. No space and carriage return & line feed on code. Only allowed in data.
+// 3. Omit "" variable definition.
+
+class Jcodd {
+
+    /**
+     * Characterize JSON
+     * 
+     * @param {string} json 
+     * 
+     * @returns {string} jcodd
+     */
+    static toCodd (json) {
+        var ex;
+        //Get clean json
+        let p1 = JSON.stringify(JSON.parse(json));
+        //Convert null to n
+        let p2 = p1.replace(/([\[\,\:])null([\]\,\}])/g, "$1n$2").replace(/([\[\,\:])null([\]\,\}])/g, "$1n$2");
+        //Convert true to t
+        let p3 = p2.replace(/([\[\,\:])true([\]\,\}])/g, "$1t$2").replace(/([\[\,\:])true([\]\,\}])/g, "$1t$2");
+        //Convert false to f
+        let p4 = p3.replace(/([\[\,\:])false([\]\,\}])/g, "$1f$2").replace(/([\[\,\:])false([\]\,\}])/g, "$1f$2");
+        //Rem4ve ""
+        let p5 = p4.replace(/([\{\,])\"([^\"]*)\"\:/g, "$1$2:");
+        //Check convert unicode
+        if (p5.match(/[\u0000-\u001F|\u0080-\uFFFF]/g) != null) {
+            let p6 = this.escape(p5);
+            ex = p6;
+        } else ex = p5;
+
+        // console.log(p1);
+        // console.log(p2);
+        // console.log(p3);
+        // console.log(ex);
+
+        return ex;
+    }
+
+    /**
+     * Convert object to JCODD directly
+     * 
+     * @param {object} obj 
+     * 
+     * @returns {string} JCODD
+     */
+    static coddify (obj) {
+        let json = JSON.stringify(obj);
+
+        return this.toCodd(json);
+    }
+
+    /**
+     * Parse JCODD to JSON
+     * 
+     * @param {string} codd 
+     * 
+     * @return {string} json
+     */
+    static toJson (codd) {
+        //unescape
+        let p1 = this.unescape(codd);//unescape(codd);//=> deprecated
+        //Assign ""
+        let p2 = p1.replace(/(\{|\}\,|\]\,|\"\,|[eE]?[+\-]?[\d.]+\,|[ntf]\,|true\,|false\,)([^\"\{\}\[\]\,\:]*)\:/g, '$1"$2":');
+        //Convert n to null
+        let p3 = p2.replace(/([\[\,\:])n([\]\,\}])/g, "$1null$2").replace(/([\[\,\:])n([\]\,\}])/g, "$1null$2");
+        //Convert t to true
+        let p4 = p3.replace(/([\[\,\:])t([\]\,\}])/g, "$1true$2").replace(/([\[\,\:])t([\]\,\}])/g, "$1true$2");
+        //Convert f to false
+        let p5 = p4.replace(/([\[\,\:])f([\]\,\}])/g, "$1false$2").replace(/([\[\,\:])f([\]\,\}])/g, "$1false$2");
+
+        return p5;
+    }
+
+    /**
+     * Convert JCODD to object directly
+     * 
+     * @param {string} codd 
+     * 
+     * @returns {*} object
+     */
+    static parse (codd) {
+        let json = this.toJson(codd);
+
+        return JSON.parse(json);
+    }
+
+    /**
+     * Return to be escaped unicode character from char code
+     * 
+     * @param {Integer} cc  Char Code
+     * 
+     * @returns {String} unescaped
+     */
+    static esc (cc) {
+        if (cc < 0x20 || cc > 0x7e) {
+            let x16 = cc.toString(16);
+            var ex;
+            if (x16.length > 2) ex = "%u" + x16.padStart(4, '0').toUpperCase();
+            else ex = "%" + x16.padStart(2, '0').toUpperCase();
+            return ex;
+        } else return String.fromCharCode(cc);
+    }
+
+    /**
+     * Return to be escaped unicode characters in string
+     * 
+     * @param {String} str
+     * 
+     * @returns {String} unescaped
+     */
+    static escape (str) {
+        var escaped = "";
+        for (var i=0; i<str.length; i++) {
+            escaped += this.esc(str.charCodeAt(i));
+        }
+        return escaped;
+    }
+
+    /**
+     * Return to be unescaped unicode characters in string
+     * 
+     * @param {String} str
+     * 
+     * @returns {String} escaped
+     */
+    static unescape (str) {
+        return str.replace(/%u([\dA-F]{4})/gi, (match, block) => 
+            String.fromCharCode(parseInt(block, 16))
+        );
+    }
+
+
+    #obj;
+    get obj() { return this.#obj; }
+    get json() { return JSON.stringify(this.#obj); }
+    get jcodd() { return Jcodd.coddify(this.#obj); }
+    get base64() { return btoa(Jcodd.coddify(this.#obj)); }
+
+
+    /**
+     * Quick set object and get converted to any data type
+     * @param {*} any Jcodd or JSON or object or primitive
+     */
+    constructor(any) {
+        if (typeof any == "string") try {
+            any = Jcodd.parse(any);
+        } catch (e) {
+            try {
+                any = Jcodd.parse(atob(any));
+            } catch (e) {
+                // do nothing
+            }
+        }
+        this.#obj = any;
+    }
+
+    toString() {
+        return this.jcodd;
+    }
+
+}
+
+const JCODD = any => new Jcodd(any);
